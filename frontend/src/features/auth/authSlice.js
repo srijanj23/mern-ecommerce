8e6dabf99be654e18d7e356b2f243b6f3605dc33
srijanj23 <95972162+srijanj23@users.noreply.github.com>
2025-10-28 15:29:08 +0530

    Update authSlice.js

--- frontend/src/features/auth/authSlice.js
+++ frontend/src/features/auth/authSlice.js
@@ -1,7 +1,7 @@
 import { createSlice, createAsyncThunk } from '@reduxjs/toolkit';
 import axios from 'axios';
 
-const API_URL = 'https://mern-ecommerce-5k9a.onrender.com/api/auth/';
+const API_URL = 'https://mern-ecommerce-5k9a.onrender.com/api/auth';
 
 // Register user
 export const register = createAsyncThunk(
